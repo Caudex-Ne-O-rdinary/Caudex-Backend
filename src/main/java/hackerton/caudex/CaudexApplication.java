c0e262115bb91ccd6d00bcf3281bf25ef4842b57
kkangmen <pickofee@gmail.com>
2026-05-16 17:06:02 +0900

    Initial Commit

---
+++ src/main/java/hackerton/caudex/CaudexApplication.java
@@ -0,0 +1,13 @@
+package hackerton.caudex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaudexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaudexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hackerton/caudex/CaudexApplicationTests.java
@@ -0,0 +1,13 @@
+package hackerton.caudex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaudexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
