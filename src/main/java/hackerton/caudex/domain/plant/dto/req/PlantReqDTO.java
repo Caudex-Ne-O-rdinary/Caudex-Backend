efd8c30b84e495281fef9bfc21b093f1d61f0a8b
kkangmen <pickofee@gmail.com>
2026-05-17 07:36:31 +0900

    Fix: DTO 검증 예외 처리 추가

--- src/main/java/hackerton/caudex/domain/plant/dto/req/PlantReqDTO.java
+++ src/main/java/hackerton/caudex/domain/plant/dto/req/PlantReqDTO.java
@@ -2,13 +2,21 @@ package hackerton.caudex.domain.plant.dto.req;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class PlantReqDTO {
+
     public record PlantUploadDTO(
+
         @NotBlank(message="식물 이름을 입력해주세요.")
+        @Size(max = 10, message = "최대 10자까지 입력할 수 있어요")
         String name,
+
+
         @NotBlank(message="관리 TIP을 입력해주세요.")
+        @Size(max = 100, message = "최대 100자까지 입력할 수 있어요")
         String managementTip,
+
         @NotNull(message="식물의 X 좌표는 반드시 존재해야 합니다.")
         Double ratioX,
         @NotNull(message="식물의 Y 좌표는 반드시 존재해야 합니다.")
