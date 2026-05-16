44f529e3e2fa40a44ee65fb0838a84afce576e3e
kkangmen <pickofee@gmail.com>
2026-05-17 05:05:26 +0900

    DTO 검증 기능

--- src/main/java/hackerton/caudex/domain/diary/dto/DiaryReqDto.java
+++ src/main/java/hackerton/caudex/domain/diary/dto/DiaryReqDto.java
@@ -1,5 +1,6 @@
 package hackerton.caudex.domain.diary.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 public class DiaryReqDto {
@@ -7,6 +8,8 @@ public class DiaryReqDto {
     // 다이어리 생성
     @Builder
     public record CreateDiary(
+
+            @Size(max = 100, message = "최대 100자까지 입력할 수 있어요.")
             String context
     ){}
 }
